8da1527d562b7f2282e7c3030b0a20c989b64add
Viktor Mitin <viktor.mitin.19@gmail.com>
2019-07-16 14:45:46 +0300

    pack_initramfs.sh: removed -a 0x4b000000 -e 0x4b000000 as not needed

--- pack_initramfs.sh
+++ pack_initramfs.sh
@@ -6,5 +6,8 @@ cd ramdisk
 find . | cpio --create --format='newc' | gzip > ../ramdisk.img
 
 cd ..
-mkimage -A arm64 -O linux -T ramdisk -C none -a 0x4b000000 -e 0x4b000000 \
+
+#mkimage -A arm64 -O linux -T ramdisk -C none -a 0x4b000000 -e 0x4b000000 \
+#       -n "Modified ramdisk" -d ./ramdisk.img ./uramdisk.img
+mkimage -A arm64 -O linux -T ramdisk -C none \
        -n "Modified ramdisk" -d ./ramdisk.img ./uramdisk.img
